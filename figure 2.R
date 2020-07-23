702830c13eedcf1bd4443a7b4446c574f02f4be9
Susan Jarvis <susjar@ceh.ac.uk>
2020-07-23 12:45:09 +0100

    Add second plot for large sample size scenario

--- figure 2.R
+++ figure 2.R
@@ -219,3 +219,75 @@ p4l <- grid.arrange(p1l, p3l, p5l, legl, ncol=1, nrow = 4,
 
 ggsave("Large scenario v2.png", p4l, device = "png", width = 15, height = 18.5, units = "cm")
 
+
+
+
+sub2l <- sim_res[sim_res$Model %in% c("structured", "unstructured", "joint", "covariate", "correlation_str") & sim_res$Scenario %in% c("Large","Default"),]
+
+p1l <- ggplot(sub2l, aes(y = MAE, x = Model), group = Scenario) +
+  geom_boxplot(aes(fill = Scenario), outlier.shape = NA) +
+  #facet_wrap(. ~ section, scales = "free") +
+  labs(fill = "Scenario", y = "MAE \n", tag = "(a)") +
+  scale_x_discrete(labels=c("correlation_str" = "Correlation", "covariate" = "Covariate","joint" = "Joint likelihood", "structured" = "Structured only", "unstructured" = "Unstructured only"))+
+  ylim(0,6) +
+  theme_classic()+
+  theme(legend.position = "bottom") +
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_text(size = 10),
+        legend.text = element_text(size = 10),
+        axis.text = element_text(size = 10),
+        axis.title = element_text(size = 10),
+        axis.title.x = element_blank())
+
+p3l <- ggplot(sub2l, aes(y = correlation, x = Model), group = Scenario) +
+  geom_boxplot(aes(fill = Scenario), outlier.shape = NA) +
+  #facet_wrap(. ~ section, scales = "free") +
+  labs(fill = "Scenario", y = "Correlation", tag = "(b)") +
+  scale_x_discrete(labels=c("correlation_str" = "Correlation", "covariate" = "Covariate","joint" = "Joint likelihood", "structured" = "Structured only", "unstructured" = "Unstructured only"))+
+  theme_classic()+
+  theme(legend.position = "none") +
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_text(size = 10),
+        legend.text = element_text(size = 10),
+        axis.text = element_text(size = 10),
+        axis.title = element_text(size = 10),
+        axis.title.x = element_blank())
+
+sub2el <- env_res[env_res$model %in% c("structured", "unstructured", "joint", "covariate", "correlation") & env_res$Scenario %in% c("Large", "Default"),]
+
+p5l <- ggplot(sub2el, aes(y = mean, x = model), group = Scenario) +
+  geom_boxplot(aes(fill = Scenario), outlier.shape = NA) +
+  #facet_wrap(. ~ section, scales = "free") +
+  labs(fill = "Scenario", y = "Coefficient estimate\n", x = "Model", tag = "(c)") +
+  scale_x_discrete(labels=c("correlation" = "Correlation", "covariate" = "Covariate","joint" = "Joint likelihood", "structured" = "PA only", "unstructured" = "PO only"))+
+  ylim(-6,9)+
+  theme_classic()+
+  theme(legend.position = "none") +
+  theme(axis.text.x = element_text(size = 10, angle = 45, vjust = 1, hjust = 1),
+        axis.text.y = element_text(size = 10),
+        legend.text = element_text(size = 10),
+        axis.text = element_text(size = 10),
+        axis.title = element_text(size = 10),
+        plot.tag = element_text(size = 10))+
+  geom_hline(yintercept = 1.2, linetype = "dashed")
+
+get_legend<-function(myggplot){
+  tmp <- ggplot_gtable(ggplot_build(myggplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+}
+
+
+legl <- get_legend(p1l)
+
+p1l <- p1l + theme(legend.position = "none")
+
+library("gridExtra")
+#p5 <- grid.arrange(p1,p2,p3,p4, ncol = 2, nrow = 2)
+p4l <- grid.arrange(p1l, p3l, p5l, legl, ncol=1, nrow = 4, 
+                    layout_matrix = rbind(c(1),c(2), c(3), c(4)),
+                    widths = c(2.7), heights = c(2, 2, 3.3, 0.2))
+
+ggsave("Large scenario plot 2 v2.png", p4l, device = "png", width = 15, height = 18.5, units = "cm")
+
